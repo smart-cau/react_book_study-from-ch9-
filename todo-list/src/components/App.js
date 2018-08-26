54d29ee417a59cf189fb423efc11ae40c61e7511
HyeonWoo <1995hyunwoo@naver.com>
2018-08-27 00:43:37 +0900

    MFK webpack.config.dev.js & PageTemplate

---
+++ todo-list/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import PageTemplate from "./PageTemplate";
+
+class App extends Component {
+  render() {
+    return <PageTemplate>일정관리</PageTemplate>;
+  }
+}
+
+export default App;
