1a56e45d5c47307f4d7159e0c4c3832d432aff4c
HyeonWoo <1995hyunwoo@naver.com>
2018-08-28 23:24:52 +0900

    10.3 1)텍스트 입력 관련 상태 관리 & 2) 초기 일정 데이터 정의 및 렌더링

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -4,11 +4,32 @@ import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 
 class App extends Component {
+  state = {
+    input: "", // input 값.
+    // 일정 데이터 초기값.
+    todos: [
+      { id: 0, text: "리액트 공부하기", done: true },
+      { id: 1, text: "컴포넌트 스타일링 해보기", done: false }
+    ]
+  };
+
+  handleChange = e => {
+    const { value } = e.target; // 비구조화 할당 사용! 이거 유용하네
+
+    this.setState({
+      input: value
+    });
+  };
+
   render() {
+    const { input, todos } = this.state; // const input = this.state.input;
+    const { handleChange } = this; // const handleChange = this.handleChange;
+
     return (
       <PageTemplate>
-        <TodoInput />
-        <TodoList />
+        {/* onChange와 value의 props를 전달! props와 상태에 대해 조금씩 알아가는 것 같다. */}
+        <TodoInput onChange={handleChange} value={input} />
+        <TodoList todos={todos} />
       </PageTemplate>
     );
   }
--- todo-list/src/components/TodoList/TodoList.js
+++ todo-list/src/components/TodoList/TodoList.js
@@ -3,13 +3,13 @@ import TodoItem from "../TodoItem";
 
 class TodoList extends Component {
   render() {
-    return (
-      <div>
-        {/* <TodoItem>여기 사이에 있는 내용이 children으로 들어간다!! 이 사이에 내용을 넣을려면 children props는 필수</TodoItem> */}
-        <TodoItem done>리액트 공부하기</TodoItem>
-        <TodoItem>리액트 스타일링 해보기</TodoItem>
-      </div>
-    );
+    const { todos } = this.props;
+    const todoList = todos.map(todo => (
+      <TodoItem key={todo.id} done={todo.done}>
+        {todo.text}
+      </TodoItem>
+    ));
+    return <div>{todoList}</div>;
   }
 }
 
