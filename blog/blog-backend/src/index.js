33becad8202b9ed617ee61dd57286e774227c6cb
HyeonWoo <1995hyunwoo@naver.com>
2018-10-03 21:56:29 +0900

    18.5.1 기본 사용법 ~ 18.5.5.1 posts 라우트 생성(postman 사용)

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,26 +1,37 @@
 const Koa = require("koa");
+const Router = require("koa-router");
+
+// Router 모듈화
+const api = require("./api");
 
 const app = new Koa();
+const router = new Router();
 
-// asinc & await 사용.
-app.use(async (ctx, next) => {
-  console.log(1);
-  // next()는 promise다! next를 실행하면 prmoise를 반환함.
-  // next().then(() => {
-  //   console.log("bye");
-  // });
-  await next();
-  console.log("bye");
+// 라우터 설정
+router.use("/api", api.routes()); // api 라우트 적용.
+
+/* 18.5.2 Route Parameter와 query
+
+router.get("/", ctx => {
+  ctx.body = "Home";
+});
+// parameters는 ctx.params로 접근 가능.
+router.get("/about/:name?", ctx => {
+  const { name } = ctx.params;
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = name ? `${name}의 소개` : `소개`;
 });
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
+// query는 query의 값이 문자열이라면 ctx.querystring으로,
+// 문자열이 아니라면 ctx.query로 접근 가능.
+router.get("/posts", ctx => {
+  const { id } = ctx.query;
+  ctx.body = id ? `Post # ${id}` : "there is no post id";
 });
+*/
 
-app.use(ctx => {
-  ctx.body = "hello world";
-});
+// app 인스턴스에 router 적용.
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log("listening to port 4000");
