e9be1e865ce0214828b97d41b1e50e3fd55b60c5
HyeonWoo <1995hyunwoo@naver.com>
2018-10-04 15:03:26 +0900

    Ch 18.5.5.2 컨트롤러 파일 작성.

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,20 +1,17 @@
+/*  src/api/posts/posts.crtrl.js에서 exports한 함수는 다음과 같이 사용할 수 있다.
+    const 모듈이름 = require('파일이름);
+    모듈이름.이름();
+*/
 const Router = require("koa-router");
+const postsCtrl = require("./posts.ctrl");
 
 const posts = new Router();
 
-const printInfo = ctx => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params
-  };
-};
-
-posts.get("/", printInfo);
-posts.post("/", printInfo);
-posts.get("/:id", printInfo);
-posts.delete("/:id", printInfo);
-posts.put("/:id", printInfo);
-posts.patch("/:id", printInfo);
+posts.get("/", postsCtrl.list);
+posts.post("/", postsCtrl.write);
+posts.get("/:id", postsCtrl.read);
+posts.delete("/:id", postsCtrl.remove);
+posts.put("/:id", postsCtrl.replace);
+posts.patch("/:id", postsCtrl.update);
 
 module.exports = posts;
--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,13 @@
+/*  18.5.5.2 컨트롤러 파일 작성
+    라우트를 작성할 때, posts.get('/', ctx => {...})에서 
+    {...} 부분이 너무 길어지면 코드 가독성과 관리가 힘들다.
+    그래서 {...} 이 부분을 따로 분리해서 관리하는데,
+    이 라우트 처리 함수{...}만 모아 놓은 파일을 '컨트롤러'라고 한다!
+    컨트롤러에서는 백엔드 기능을 구현한다.
+*/
 const Koa = require("koa");
 const Router = require("koa-router");
+const bodyparser = require("koa-bodyparser");
 
 // Router 모듈화
 const api = require("./api");
@@ -10,25 +18,7 @@ const router = new Router();
 // 라우터 설정
 router.use("/api", api.routes()); // api 라우트 적용.
 
-/* 18.5.2 Route Parameter와 query
-
-router.get("/", ctx => {
-  ctx.body = "Home";
-});
-// parameters는 ctx.params로 접근 가능.
-router.get("/about/:name?", ctx => {
-  const { name } = ctx.params;
-  // name의 존재 유무에 따라 다른 결과 출력
-  ctx.body = name ? `${name}의 소개` : `소개`;
-});
-
-// query는 query의 값이 문자열이라면 ctx.querystring으로,
-// 문자열이 아니라면 ctx.query로 접근 가능.
-router.get("/posts", ctx => {
-  const { id } = ctx.query;
-  ctx.body = id ? `Post # ${id}` : "there is no post id";
-});
-*/
+app.use(bodyparser());
 
 // app 인스턴스에 router 적용.
 app.use(router.routes()).use(router.allowedMethods());
