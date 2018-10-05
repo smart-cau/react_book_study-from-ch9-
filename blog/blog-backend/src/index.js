d367fb8f1dc70bbebd278936ca3907a4805225c5
HyeonWoo <1995hyunwoo@naver.com>
2018-10-05 15:44:37 +0900

    Ch 19.3.1 .env 환경변수 파일 생성 & Ch 19.3.2 mongoose로 db에 연결

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,13 +1,33 @@
-/*  18.5.5.2 컨트롤러 파일 작성
-    라우트를 작성할 때, posts.get('/', ctx => {...})에서 
-    {...} 부분이 너무 길어지면 코드 가독성과 관리가 힘들다.
-    그래서 {...} 이 부분을 따로 분리해서 관리하는데,
-    이 라우트 처리 함수{...}만 모아 놓은 파일을 '컨트롤러'라고 한다!
-    컨트롤러에서는 백엔드 기능을 구현한다.
-*/
+// .env파일. 이 파일에는 서버에서 사용할 포트와 Mongo DB 주소를 넣어놨음. DB 이름은 blog.
+// .env 파일 적용. Node.js에서 환경변수는 process.env파일로 조회 가능.
+require("dotenv").config();
+
 const Koa = require("koa");
 const Router = require("koa-router");
 const bodyparser = require("koa-bodyparser");
+
+const mongoose = require("mongoose");
+
+// 비구조화 할당 문법으로 process.env 파일 내부 값에 대한 레퍼런스를 만듦.
+const {
+  PORT: port = 4000, // 값이 존재하지 않는다면 4000을 기본 값으로 사용.
+  MONGO_URI: mongoURI
+} = process.env;
+
+// mongoose를 사용하여 서버에 DB 연결.
+mongoose.Promise = global.Promise; // Node의 Promise를 사용하도록 설정. (자세한 설명 = 447p)
+mongoose.Promise = global.Promise; // Node의 Promise를 사용하도록 설정
+mongoose
+  .connect(
+    mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(e => {
+    console.error(e);
+  });
 
 // Router 모듈화
 const api = require("./api");
@@ -23,6 +43,6 @@ app.use(bodyparser());
 // app 인스턴스에 router 적용.
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log("listening to port 4000");
+app.listen(port, () => {
+  console.log("listening to port", port);
 });
