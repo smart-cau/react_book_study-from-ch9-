820a520d8b37213e255ebfb9c945b2ab0ef944e9
HyeonWoo <1995hyunwoo@naver.com>
2018-10-10 12:22:21 +0900

    Ch 20.5.2 EditorHeader 컴포넌트 작성.

--- blog/blog-frontend/src/pages/EditorPage.js
+++ blog/blog-frontend/src/pages/EditorPage.js
@@ -1,8 +1,15 @@
 import React from "react";
 import EditorTemplate from "components/editor/EditorTemplate";
+import EditorHeader from "components/editor/EditorHeader";
 
 const EditorPage = () => {
-  return <EditorTemplate header="header" editor="editor" preview="preview" />;
+  return (
+    <EditorTemplate
+      header={<EditorHeader />}
+      editor="editor"
+      preview="preview"
+    />
+  );
 };
 
 export default EditorPage;
